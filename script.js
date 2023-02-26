101721900defc84faea6fff89225ef4f460a5116
Jorge Federico Miranda Morones <jorge100099@hotmail.com>
2023-02-26 13:06:53 -0600

    Clase03 completada

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const btn=document.querySelector("[data-form-btn]");
-const input= document.querySelector("[data-form-input]")
+
 
 const createTask= (evento)=>{
     evento.preventDefault();
-    console.log("crear tarea")
-    console.log(input.value)
+    const input= document.querySelector("[data-form-input]");
+    const value=input.value;
+    const list= document.querySelector("[data-list]")
+    const task= document.createElement('li');
+    task.classList.add('card')
+    input.value="";
+    const content=`<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+  task.innerHTML=content;
+  list.appendChild(task)
 }
 
 btn.addEventListener("click",createTask)
